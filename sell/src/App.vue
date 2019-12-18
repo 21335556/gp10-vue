b0f049a05cd170abccfdd023b82ab8b4bdf10b74
李某人氏 <1987368180@qq.com>
2019-12-18 20:20:06 +0800

    sell项目vue之前版本;未完成

---
+++ sell/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <headers></headers>
+    <div class="header">
+      I am header
+    </div>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+import headers from './components/header/header'
+
+export default {
+  components: {
+    headers
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="headers">
+    我是header
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus">
+
+</style>
